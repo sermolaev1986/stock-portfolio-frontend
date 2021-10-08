d30459b243629519be097e866405d3e32b897f30
Sergei Ermolaev <sergei.ermolaev@t-mobile.at>
2021-10-08 15:16:06 +0200

    changed path back to prod

--- src/app/constants/paths.ts
+++ src/app/constants/paths.ts
@@ -1,5 +1,5 @@
 export abstract class Paths {
-  static readonly HEROKU_API_PATH = "http://localhost:9999/v1";
-  // static readonly HEROKU_API_PATH = "https://chubr-stock-portfolio.herokuapp.com/v1";
+  // static readonly HEROKU_API_PATH = "http://localhost:9999/v1";
+  static readonly HEROKU_API_PATH = "https://chubr-stock-portfolio.herokuapp.com/v1";
 
 }
