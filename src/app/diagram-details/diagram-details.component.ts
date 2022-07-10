2127d0279ec63f1173eabef29bf3c2a5a44956af
Sergei Ermolaev <sergei.ermolaev@t-mobile.at>
2022-07-10 14:33:45 +0200

    - added diagram components

---
+++ src/app/diagram-details/diagram-details.component.ts
@@ -0,0 +1,146 @@
+import {Component, OnInit} from '@angular/core';
+import {ChartData} from "../portfolio/data";
+
+@Component({
+  selector: 'app-diagram-details',
+  templateUrl: './diagram-details.component.html',
+  styleUrls: ['./diagram-details.component.css']
+})
+export class DiagramDetailsComponent implements OnInit {
+
+  private originalData: Array<ChartData> = [];
+  public chartData: any = [];
+
+  public selectedTypes: string[] = ['stock', 'bond', 'reit'];
+
+  public chartType = 'pie';
+  public chartTypes = [
+    {name: 'Pie', code: 'pie'},
+    {name: 'Doughnut', code: 'doughnut'},
+    {name: 'Bar', code: 'bar'}
+  ];
+
+  public displayGroupedByType = false;
+  public displayDataLabels = true;
+
+  ngOnInit(): void {
+    this.originalData = history.state.originalData;
+    this.chartData = this.getPieChartData(this.originalData);
+  }
+
+  private getPieChartDataGroupedByType(data: Array<ChartData>) {
+    let map = this.groupBy(data, value => value.type);
+
+    return {
+      labels: Array.from(map.keys()),
+      datasets: [
+        {
+          data: Array.from(map.values()),
+          backgroundColor: [
+            "#42A5F5",
+            "#66BB6A",
+            "#FFA726",
+            "#62929E",
+            "#4A6D7C",
+            "#9AD1D4",
+            "#FCE762",
+            "#4F4789",
+            "#CBE896",
+            "#AD5D4E",
+            "#EB6534",
+            "#E85F5C",
+            "#FFDAC6",
+            "#7C6A0A"
+          ],
+          hoverBackgroundColor: [
+            "#64B5F6",
+            "#81C784",
+            "#FFB74D",
+            "#62929E",
+            "#4A6D7C",
+            "#9AD1D4",
+            "#FCE762",
+            "#4F4789",
+            "#CBE896",
+            "#AD5D4E",
+            "#EB6534",
+            "#E85F5C",
+            "#FFDAC6",
+            "#7C6A0A"
+          ]
+        }
+      ]
+    };
+  }
+
+  private groupBy(list: Array<ChartData>, keyGetter) {
+    const map: Map<string, number> = new Map();
+    list.forEach((item) => {
+      const key = keyGetter(item);
+      const price = map.get(key);
+      if (!price) {
+        map.set(key, item.price);
+      } else {
+        map.set(key, item.price + price);
+      }
+    });
+    return map;
+  }
+
+  public toggleDisplayGroupedByType() {
+    if (this.displayGroupedByType) {
+      this.chartData = this.getPieChartDataGroupedByType(this.originalData);
+    } else {
+      this.chartData = this.getPieChartData(this.originalData);
+    }
+  }
+
+  private getPieChartData(data: Array<ChartData>) {
+    return {
+      labels: data.map(item => item.name),
+      datasets: [
+        {
+          data: data.map(item => item.price),
+          backgroundColor: [
+            "#42A5F5",
+            "#66BB6A",
+            "#FFA726",
+            "#62929E",
+            "#4A6D7C",
+            "#9AD1D4",
+            "#FCE762",
+            "#4F4789",
+            "#CBE896",
+            "#AD5D4E",
+            "#EB6534",
+            "#E85F5C",
+            "#FFDAC6",
+            "#7C6A0A"
+          ],
+          hoverBackgroundColor: [
+            "#64B5F6",
+            "#81C784",
+            "#FFB74D",
+            "#62929E",
+            "#4A6D7C",
+            "#9AD1D4",
+            "#FCE762",
+            "#4F4789",
+            "#CBE896",
+            "#AD5D4E",
+            "#EB6534",
+            "#E85F5C",
+            "#FFDAC6",
+            "#7C6A0A"
+          ]
+        }
+      ]
+    };
+  }
+
+  public refreshPieChart() {
+    this.chartData = this.getPieChartData(this.originalData.filter(value => this.selectedTypes.includes(value.type)));
+    this.displayDataLabels = this.chartType != 'bar';
+  }
+
+}
---
+++ src/app/diagram/diagram.component.ts
@@ -0,0 +1,72 @@
+import {Component, Input, OnInit} from '@angular/core';
+import ChartDataLabels from "chartjs-plugin-datalabels";
+
+@Component({
+  selector: 'app-diagram',
+  templateUrl: './diagram.component.html',
+  styleUrls: ['./diagram.component.css']
+})
+export class DiagramComponent implements OnInit {
+
+  @Input()
+  chartType: string;
+
+  @Input()
+  public chartData: any = [];
+
+  @Input()
+  public displayDataLabels = false;
+
+  public plugins = [ChartDataLabels];
+  public options = {
+    indexAxis: 'y',
+    plugins: {
+      datalabels: {
+        enabled: true,
+        display: true,
+        /* show value in percents */
+        formatter: (value, ctx) => {
+          if (!this.displayDataLabels) {
+            return '';
+          }
+
+          const label = ctx.chart.data.labels[ctx.dataIndex];
+
+          let sum = 0;
+          const dataArr = ctx.chart.data.datasets[0].data;
+          dataArr.map(data => {
+            sum += data;
+          });
+          const percentage = (value * 100 / sum);
+          if (percentage < 1.5) {
+            return '';
+          } else {
+            let name = percentage.toFixed(2);
+            if (percentage > 2) {
+              name = name + '%';
+            }
+
+            if (percentage > 3) {
+              name = label + ' ' + name;
+            }
+
+            return name;
+          }
+
+        },
+        color: '#fff',
+      },
+      legend: {
+        display: false
+      },
+      responsive: true
+    }
+  };
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
