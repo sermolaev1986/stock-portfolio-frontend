5c79bef1d34a4bcdd9b581306c1817ddba528354
Sergei Ermolaev <sergei.ermolaev@t-mobile.at>
2022-07-29 11:03:25 +0200

    - trying to fix server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static('dist/<app-name>'));
+app.use(express.static(__dirname + '/dist/<app-name>'));
 app.get('/*', function (req, res) {
-  res.sendFile(path.join('dist/<app-name>/index.html'));
+  res.sendFile(path.join(__dirname +
+    '/dist/<app-name>/index.html'));
 });
 app.listen(process.env.PORT || 8080);
