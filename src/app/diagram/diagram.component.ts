c4380594796c0b005f95e09fc898f9f92f0b7acd
Sergei Ermolaev <sergei.ermolaev@magenta.at>
2022-08-02 13:57:55 +0200

    added last year averages to comparison

--- src/app/diagram/diagram.component.ts
+++ src/app/diagram/diagram.component.ts
@@ -12,6 +12,9 @@ export class DiagramComponent implements OnInit {
   width: string = "300px";
 
   @Input()
+  height: string = "300px";
+
+  @Input()
   chartType: string;
 
   @Input()
@@ -26,6 +29,48 @@ export class DiagramComponent implements OnInit {
   public plugins = [ChartDataLabels];
   public options = {
     indexAxis: 'y',
+    minBarLength: 100,
+    scales: {
+      y: {
+        grid: {
+          display: false,
+          drawBorder: false
+        },
+        showTicks: false,
+        categoryPercentage: 1,
+        barPercentage: 1,
+        showTickMarks: false,
+        display: true,
+        scaleLabel: {
+          show: false,
+          labelString: 'Value'
+        },
+        ticks: {
+          // callback: function(value, index, ticks) {
+          //   return index;
+          // },
+          mirror: false,
+          display: false,
+          beginAtZero: true,
+          max: 100,
+          min: 0,
+        }
+      },
+      x: {
+        grid: {
+          display: false,
+          drawBorder: false
+        },
+        showTicks: false,
+        categoryPercentage: 0.9,
+        showTickMarks: false,
+        barPercentage: 0.9,
+        ticks: {
+          display: false,
+          beginAtZero: 0
+        }
+      }
+    },
     plugins: {
       datalabels: {
         enabled: true,
@@ -52,8 +97,29 @@ export class DiagramComponent implements OnInit {
         /* show value in percents */
         // @ts-ignore
         this.options.plugins.datalabels.formatter = this.format;
+      } else {
+        // @ts-ignore
+        this.options.plugins.datalabels.formatter = this.formatWithLabel;
       }
     }
+  }
+
+  formatWithLabel(value, ctx): string {
+    const label = ctx.chart.data.labels[ctx.dataIndex];
+
+    // let sum = 0;
+    // const dataArr = ctx.chart.data.datasets[0].data;
+
+    if (value === 0) {
+      return "                —";
+    }
+
+    if (value < 100) {
+      return value;
+    } else {
+      return label + " " + value;
+    }
+
   }
 
   formatNoOp(value, ctx): string {
--- src/app/expenses/expenses.component.ts
+++ src/app/expenses/expenses.component.ts
@@ -12,8 +12,8 @@ export class ExpensesComponent implements OnInit {
   otherMonth: Date;
 
   public colorsPool = [
-    {backgroundColor: "red", hoverColor: "red-lighter"},
-    {backgroundColor: "green", hoverColor: "green-lighter"}
+    {backgroundColor: "red", hoverColor: "light-red"},
+    {backgroundColor: "green", hoverColor: "light-green"}
   ];
 
   constructor(private readonly expensesService: ExpensesService) {
@@ -38,33 +38,40 @@ export class ExpensesComponent implements OnInit {
       const values: Expense[][] = [];
       for (let key in expensesMap) {
         keys.push(key);
-        values.push(expensesMap[key]);
+        if (key !== "lastYearAverages") {
+          values.push(expensesMap[key]);
+        }
       }
 
-      expensesMap[month].sort((a, b) => b.value - a.value);
+      // expensesMap[month].sort((a, b) => b.value - a.value);
+      //
+      // const myMap = new Map();
+      // expensesMap[month].forEach((object, index) => {
+      //   myMap.set(object.category, index);
+      // });
+      //
+      // if (expensesMap[otherMonth]) {
+      //   expensesMap[otherMonth].sort((a, b) => {
+      //     return myMap.get(a.category) - myMap.get(b.category)
+      //   })
+      // }
 
-      const myMap = new Map();
-      expensesMap[month].forEach((object, index) => {
-        myMap.set(object.category, index);
+      const datasets = values.map((expenses, index) => {
+        return {
+          data: expenses.map(expense => expense.value),
+          backgroundColor: this.colorsPool[index].backgroundColor,
+          hoverBackgroundColor: this.colorsPool[index].hoverColor,
+        };
+      })
+
+      datasets.push({
+        data: expensesMap["lastYearAverages"].map(expense => expense.value),
+        backgroundColor: "#1E555C",
+        hoverBackgroundColor: "light-blue",
       });
-
-      console.log(myMap);
-
-      if (expensesMap[otherMonth]) {
-        expensesMap[otherMonth].sort((a, b) => {
-          return myMap.get(a.category) - myMap.get(b.category)
-        })
-      }
-
       this.chartData = {
-        labels: expensesMap[month].map(value => value.category),
-        datasets: values.map((expenses, index) => {
-          return {
-            data: expenses.map(expense => expense.value),
-            backgroundColor: this.colorsPool[index].backgroundColor,
-            hoverBackgroundColor: this.colorsPool[index].hoverColor,
-          };
-        })
+        labels: values[0].map(value => value.category),
+        datasets: datasets
       };
     })
   }
