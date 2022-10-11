ba55f37c36020a51c33660eb1524f3f387a41c4b
Sergei Ermolaev <sergei.ermolaev@magenta.at>
2022-10-11 15:04:50 +0200

    migrating backend to aws

--- src/app/constants/paths.ts
+++ src/app/constants/paths.ts
@@ -1,5 +1,6 @@
 export abstract class Paths {
   // static readonly HEROKU_API_PATH = "http://localhost:9999/v1";
-  static readonly HEROKU_API_PATH = "https://chubr-stock-portfolio.herokuapp.com/v1";
+  // static readonly HEROKU_API_PATH = "https://chubr-stock-portfolio.herokuapp.com/v1";
+  static readonly HEROKU_API_PATH = "http://stockportfoliobackend-env.eba-yjiuk9fs.us-east-1.elasticbeanstalk.com/v1";
 
 }
